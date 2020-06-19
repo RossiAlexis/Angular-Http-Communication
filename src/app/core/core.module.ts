42918bacc10c8d030b28ba40202be70f710187d5
alexisjrossi <alexisjrossi@gmail.com>
2020-06-19 00:24:22 -0300

    module 5: adding interceptors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,3 +1,4 @@
+import { AddHeaderInterceptor } from './add-header.interceptor';
 import { NgModule, Optional, SkipSelf, ErrorHandler } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -6,6 +7,8 @@ import { DataService } from './data.service';
 import { PlainLoggerService } from "./plain-logger.service";
 import { throwIfAlreadyLoaded } from "app/core/module-import-guard";
 import { BookTrackerErrorHandlerService } from './book-tracker-error-handler.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { LogResponseInterceptor } from './log-response.interceptor';
 
 
 @NgModule({
@@ -14,9 +17,11 @@ import { BookTrackerErrorHandlerService } from './book-tracker-error-handler.ser
   ],
   declarations: [],
   providers: [
-    LoggerService, 
-    DataService, 
-    { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService }
+    LoggerService,
+    DataService,
+    { provide: ErrorHandler, useClass: BookTrackerErrorHandlerService },
+    { provide: HTTP_INTERCEPTORS, useClass: AddHeaderInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LogResponseInterceptor, multi: true }
   ]
 })
 export class CoreModule {
@@ -24,5 +29,5 @@ export class CoreModule {
   constructor( @Optional() @SkipSelf() parentModule: CoreModule) {
     throwIfAlreadyLoaded(parentModule, 'CoreModule');
   }
-  
+
  }
